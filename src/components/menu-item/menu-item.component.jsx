3d8ab7a9a01a27186f3db2eb25399b3bed4f5f67
Sir-Dazzling <ottidan20@gmail.com>
2019-11-07 11:19:13 +0100

    designed menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,12 +5,12 @@ const MenuItem = ({title, imageUrl, size}) =>
 (
     <div className={`${size} menu-item`}>
 
-    <div className="background-image" style=
-    {
+        <div className="background-image" style=
         {
-            backgroundImage: `url(${imageUrl})`
-        }
-    } />
+            {
+                backgroundImage: `url(${imageUrl})`
+            }
+        }       />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
