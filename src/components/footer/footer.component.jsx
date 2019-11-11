d96d60bd499f83a4e7bfd6e8afa1c5ad57d10115
Sir-Dazzling <ottidan20@gmail.com>
2019-11-11 22:00:50 +0100

    Added footer

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer = ()  =>
+(
+    <footer>
+        <p className="footer-text">Designed by Dazzling Star@2019</p>
+    </footer>
+);
+
+export default Footer;
