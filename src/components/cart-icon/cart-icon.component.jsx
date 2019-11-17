a007c5b2b5ea170ea435bc51c210e9b585288e6f
Sir-Dazzling <ottidan20@gmail.com>
2019-11-17 22:06:33 +0100

    Added selectors library to the app for the cart dropdown and add to cart features

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 import {ReactComponent as ShoppingIcon} from '../../images/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
@@ -12,8 +13,8 @@ const CartIcon  = ({toggleCartHidden,itemCount}) => (
     </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-     itemCount: cartItems.reduce((accumulatedQuantity,CartItem) => accumulatedQuantity + CartItem.quantity,0)
+const mapStateToProps = (state) => ({
+     itemCount: selectCartItemsCount(state)
 });
 
 const mapDispatchToProps = (dispatch) => ({
