4dfd32ba6808c1cfa88c7d751fa2766b96dfd48c
Sir-Dazzling <ottidan20@gmail.com>
2019-11-28 16:47:34 +0100

    Added a Checkout  Component

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 import './checkout.styles.scss';
 
-const CheckoutItem = () => 
+const CheckoutItem = ({cartItem: {name,imageUrl,price,quantity}}) => 
 (
-    
+    <div className="checkout-item">
+        <div className="image-container">
+            <img src={imageUrl} alt="item" />
+        </div>
+        <span className="name">{name}</span>
+        <span className="quantity">{quantity}</span>
+        <span className="price">{price}</span>
+        <div className="remove-button">&#10005;</div>
+    </div>
 )
+
+export default CheckoutItem;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems,selectCartTotal} from '../../redux/cart/cart.selectors';
+import CheckoutItem from  '../../components/checkout/checkout.component';
 
 import './checkout.styles.scss';
 
@@ -27,7 +28,9 @@ const CheckoutPage  = ({cartItems,total}) =>
         </div>
         {
             cartItems.map(cartItem =>
-                cartItem.name
+                (
+                    <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                )
             )
         }
         <div className="total">
