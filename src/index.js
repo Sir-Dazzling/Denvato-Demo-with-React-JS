72a85e204d842e68ca1bf44e7e09ea1c9a669b82
Sir-Dazzling <ottidan20@gmail.com>
2019-11-29 14:44:15 +0100

    Configured Storage with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store,persistor} from './redux/store';
 import App from './App';
-import store from './redux/store';
+
 
 import './index.css';
 
@@ -15,7 +17,9 @@ import './index.css';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>, 
 document.getElementById('root'));
