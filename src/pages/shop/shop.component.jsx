a906939936e114e7b99137c6764db169ac2b0552
Sir-Dazzling <ottidan20@gmail.com>
2019-12-04 12:37:37 +0100

    Added a collection component and shop reducer and selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import {Route} from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => 
+const ShopPage = ({match}) => 
 (
         <div className="shop-page">
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                )
-                    
-                )
-            }
+           <Route exact path={`${match.path}`} component={CollectionsOverview} />
+           <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
         </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
